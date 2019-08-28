2a7d8bbbb2b050d871a2d10ef40f9a224e412855
root <root@debug010000002015.local.alipay.net>
2019-08-28 14:30:20 +0800

    main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import readdata
 import video
 import keyframe
 import vhash
+import check_md5
+import recognize
 
 #输入：文件名 输出：[[相似视频 相似度]...]
 def videores(src, tmp = 'data/tmp.mp4'):
@@ -14,15 +16,13 @@ def videores(src, tmp = 'data/tmp.mp4'):
     
 #输入：文件名 输出：[[相似视频 相似度]...]
 def audiores(src):
-    return []
-
-def md5check(src):
-    return None
+    return recognize.recognize_mp4(src)
 
 #输入：待测视频文件名 输出：[[[总相似文件名 相似度]...][[视频相似 相似度]...][[音频相似 相似度]...]]
 def main(src):
-    md5res = md5check(src)
-    if md5res != None:
+    md5res = check_md5.check_md5_exists(src)
+    print(md5res)
+    if False and md5res != None and md5res != '':
         return [[[md5res, 1]], [[md5res, 1]], [[md5res, 1]]]
     vr = videores(src)
     ar = audiores(src)
